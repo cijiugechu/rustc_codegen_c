1f7c884bee7b5e949e6019efc8755ea0d7db6a9a
cijiugechu <cijiugechu@gmail.com>
2026-02-12 00:06:45 +0800

    Update to nightly-2025-09-27

--- crates/rustc_codegen_c/src/write.rs
+++ crates/rustc_codegen_c/src/write.rs
@@ -1,29 +1,38 @@
 use std::fs;
 use std::io::Write;
-use std::process::Stdio;
+use std::process::{Command, Stdio};
 
-use rustc_codegen_ssa::back::command::Command;
 use rustc_codegen_ssa::back::write::{CodegenContext, ModuleConfig};
 use rustc_codegen_ssa::{CompiledModule, ModuleCodegen};
-use rustc_errors::{DiagCtxtHandle, FatalError};
+use rustc_errors::DiagCtxtHandle;
 use rustc_session::config::OutputType;
 use tracing::error;
 
-pub(crate) unsafe fn codegen(
+pub(crate) fn codegen(
     cgcx: &CodegenContext<crate::CCodegen>,
-    _dcx: DiagCtxtHandle<'_>,
     module: ModuleCodegen<String>,
     _config: &ModuleConfig,
-) -> Result<CompiledModule, FatalError> {
-    let module_name = module.name.clone();
-    let module_name = Some(&module_name[..]);
-    let obj_out = cgcx.output_filenames.temp_path(OutputType::Object, module_name);
+) -> CompiledModule {
+    let dcx = cgcx.create_dcx();
+    let dcx = dcx.handle();
+    let obj_out = cgcx.output_filenames.temp_path_for_cgu(
+        OutputType::Object,
+        &module.name,
+        cgcx.invocation_temp.as_deref(),
+    );
     let c_out = obj_out.with_extension("c");
 
     // output c source code
-    let c_out_file = fs::File::create(&c_out).map_err(|_| FatalError)?;
-    writeln!(&c_out_file, "// file: {}.c", module.name).map_err(|_| FatalError)?;
-    write!(&c_out_file, "{}", module.module_llvm).map_err(|_| FatalError)?;
+    // dcx.fatal() exits so unwrap is fine
+    let c_out_file = fs::File::create(&c_out)
+        .map_err(|err| dcx.fatal(format!("Failed to create C source file: {err}")))
+        .unwrap();
+    writeln!(&c_out_file, "// file: {}.c", module.name)
+        .map_err(|err| dcx.fatal(format!("Failed to write to C source file: {err}")))
+        .unwrap();
+    write!(&c_out_file, "{}", module.module_llvm)
+        .map_err(|err| dcx.fatal(format!("Failed to write to C source file: {err}")))
+        .unwrap();
 
     // invoke cc to compile
     // FIXME: configure cc
@@ -31,7 +40,6 @@ pub(crate) unsafe fn codegen(
     // FIXME: flush_linked_file (windows)
     let mut cmd = Command::new("clang");
     cmd.arg(&c_out).arg("-o").arg(&obj_out).arg("-c");
-    let mut cmd = cmd.command();
     let output = match cmd
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -41,23 +49,31 @@ pub(crate) unsafe fn codegen(
         Ok(output) => output,
         Err(e) => {
             error!("failed to spawn C compiler: {}", e);
-            return Err(FatalError);
+            dcx.fatal(format!("failed to spawn C compiler: {e}"));
         }
     };
 
     if !output.status.success() {
         error!("compiler stderr:\n{}", String::from_utf8_lossy(&output.stderr));
         error!("compiler stdout:\n{}", String::from_utf8_lossy(&output.stdout));
-        return Err(FatalError);
+        dcx.fatal("C compiler failed to compile module");
     }
 
-    Ok(module.into_compiled_module(true, false, false, false, false, &cgcx.output_filenames))
+    module.into_compiled_module(
+        true,
+        false,
+        false,
+        false,
+        false,
+        &cgcx.output_filenames,
+        cgcx.invocation_temp.as_deref(),
+    )
 }
 
 pub(crate) fn link(
     _cgcx: &CodegenContext<crate::CCodegen>,
     _dcx: DiagCtxtHandle<'_>,
     mut _modules: Vec<ModuleCodegen<String>>,
-) -> Result<ModuleCodegen<String>, FatalError> {
+) -> ModuleCodegen<String> {
     unimplemented!();
 }
